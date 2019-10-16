c6073c9fa7dff1bd661c05d871183a3d1c7c1f39
matt day <mattdxc982@gmail.com>
2019-10-16 00:47:56 +0000

    course creation lesson 13

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
+   namespace :instructor do
+    resources :courses, only: [:new, :create, :show]
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_16_000514) do
+ActiveRecord::Schema.define(version: 2019_10_16_004053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "courses", id: :serial, force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.decimal "cost"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_courses_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
