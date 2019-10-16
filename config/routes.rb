c3647b1b83f278822a5a77282875f6c9ba28816f
matt day <mattdxc982@gmail.com>
2019-10-16 01:01:30 +0000

    listing courses lesson 15

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
+  resources :courses, only: [:index, :show]
    namespace :instructor do
     resources :courses, only: [:new, :create, :show]
   end
