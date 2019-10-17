d1bd59766bd9fa5719f3960caac7997913d21b9d
matt day <mattdxc982@gmail.com>
2019-10-17 01:44:41 +0000

    finish permissions lesson 20

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,17 +1,29 @@
 class Instructor::SectionsController < ApplicationController
-  def new
-    @course = Course.find(params[:course_id])
+   before_action :authenticate_user!
+   before_action :require_authorized_for_current_course
+   
+   def new
     @section = Section.new
   end
-  
-    def create
-    @course = Course.find(params[:course_id])
-    @section = @course.sections.create(section_params)
-    redirect_to instructor_course_path(@course)
+
+  def create
+    @section = current_course.sections.create(section_params)
+    redirect_to instructor_course_path(current_course)
   end
 
   private
-
+  
+   def require_authorized_for_current_course
+    if current_course.user != current_user
+      render plain: "Unauthorized", status: :unauthorized
+    end
+  end
+  
+   helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:course_id])
+  end
+  
   def section_params
     params.require(:section).permit(:title)
   end
