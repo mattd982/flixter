09528c991da88d550d6b0b395f0895216ecba979
matt day <mattdxc982@gmail.com>
2019-10-25 03:07:15 +0000

    lesson 23, edit app/uploader pixel resize later

--- Gemfile
+++ Gemfile
@@ -68,6 +68,7 @@ gem 'devise'
 gem 'carrierwave', '~> 2.0'
 gem "figaro"
 gem "fog-aws"
+gem "mini_magick"
 
 source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,8 @@
 class ImageUploader < CarrierWave::Uploader::Base
+
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   #storage :file
@@ -22,7 +23,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process scale: [200, 300]
+   process resize_to_fill: [200, 300]
   #
   # def scale(width, height)
   #   # do something
