f1520fbbb70706ea34e4e3cc06df43693743342f
matt day <mattdxc982@gmail.com>
2019-10-28 23:28:46 +0000

    edit

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -5,7 +5,7 @@ class VideoUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   #storage :file
-   storage :aws
+  storage :aws
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
 # config/initializers/carrierwave.rb
 
 CarrierWave.configure do |config|
-   config.storage    = :aws
+  config.storage    = :aws
   config.aws_bucket = ENV["AWS_BUCKET"]
   config.aws_acl    = "public-read"
 
@@ -9,5 +9,5 @@ CarrierWave.configure do |config|
       access_key_id:     ENV["AWS_ACCESS_KEY"],
       secret_access_key: ENV["AWS_SECRET_KEY"],
       region:            ENV["AWS_REGION"]
-  }             
+  }
 end
