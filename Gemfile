a9f7114688a4ff3ecf28c13874c7ba69ba0e8647
matt day <mattdxc982@gmail.com>
2019-11-04 02:54:23 +0000

    lesson 30 ordering lessons

--- Gemfile
+++ Gemfile
@@ -71,6 +71,7 @@ gem "fog-aws"
 gem "mini_magick"
 gem 'carrierwave-aws'
 gem 'stripe'
+gem 'ranked-model'
 
 source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_29_001202) do
+ActiveRecord::Schema.define(version: 2019_11_04_023942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,8 @@ ActiveRecord::Schema.define(version: 2019_10_29_001202) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "video"
+    t.integer "row_order"
+    t.index ["row_order"], name: "index_lessons_on_row_order"
     t.index ["section_id"], name: "index_lessons_on_section_id"
   end
 
