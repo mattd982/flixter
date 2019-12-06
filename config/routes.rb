31739ae391115342ca07d5b20cfd657a50d04601
matt day <mattdxc982@gmail.com>
2019-12-06 01:12:00 +0000

    Lesson 34 instructor dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resource :dashboard, only: [:show]
   root 'static_pages#index'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
