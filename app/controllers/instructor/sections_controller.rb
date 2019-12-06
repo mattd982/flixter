348f1c08bedea6321a2ce5d03dd38973b5f49bbf
matt day <mattdxc982@gmail.com>
2019-12-06 00:48:15 +0000

    Lesson 33 style instructor page

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -14,7 +14,7 @@ class Instructor::SectionsController < ApplicationController
   
   def update
     current_section.update_attributes(section_params)
-    render plain: 'updated!'
+    render plain: 'ok!'
   end
 
   private
@@ -25,23 +25,22 @@ class Instructor::SectionsController < ApplicationController
     end
   end
   
+    def current_section
+        @current_section ||= Section.find(params[:id])
+    end
+  
    def require_authorized_for_current_course
-    if current_course.section.user != current_user
+    if current_course.user != current_user
       render plain: "Unauthorized", status: :unauthorized
     end
   end
   
-helper_method :current_course
-def current_course
-    if params[:course_id]
-        @current_course ||= Course.find(params[:course_id])
-    else
-        current_section.course
+  helper_method :current_course
+    def current_course
+      @current_course ||= Course.find(params[:course_id])
     end
-end
     
-  
-  def section_params
-    params.require(:section).permit(:title, :row_order_position)
-  end
-end
+    def section_params
+      params.require(:section).permit(:title, :row_order_position)
+    end
+   end
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,7 @@ Rails.application.routes.draw do
   resources :lessons, only: [:show]
   namespace :instructor do
     resources :lessons, only: [:update]
-    resources :sections, only: [] do
-    resources :sections, only: [:update] 
+    resources :sections, only: [:update] do 
       resources :lessons, only: [:new, :create]
     end
     resources :courses, only: [:new, :create, :show] do
