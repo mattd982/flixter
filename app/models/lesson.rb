7223dae8b209e4d72fd1378283cbfb173c7d4f19
matt day <mattdxc982@gmail.com>
2019-12-07 06:38:50 +0000

    Lesson 37 next lesson button

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -4,4 +4,12 @@ class Lesson < ApplicationRecord
   
   include RankedModel
   ranks :row_order, with_same: :section_id
+  
+  def next_lesson
+    lesson = section.lessons.where("row_order > ?", self.row_order).rank(:row_order).first
+    if lesson.blank? && section.next_section
+      return section.next_section.lessons.rank(:row_order).first
+    end
+  end
+  
 end
