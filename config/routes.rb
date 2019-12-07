03fb735a68c46fea647cb6f80c42994af26b410e
matt day <mattdxc982@gmail.com>
2019-12-07 06:28:40 +0000

    Lesson 36 new lesson form modal

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   namespace :instructor do
     resources :lessons, only: [:update]
     resources :sections, only: [:update] do 
-      resources :lessons, only: [:new, :create]
+      resources :lessons, only: [:create]
     end
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:create]
